7c2f70ee066b48caab6c3996995c16c3b309f587
Colin <cstatro@gmail.com>
2019-12-05 09:24:28 -0500

    ok got the array of images with headlines created

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const images = [
+  {
+    src:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=671,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.09.32-AM.png",
+    headline: "This Dog Wearing Gloves"
+  },
+  {
+    src:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=675,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.09.39-AM.png",
+    headline: "Tree With Underwear"
+  },
+  {
+    src:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=675,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.11.02-AM.png",
+    headline: "Giant Squirrel"
+  },
+  {
+    src:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=675,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.11.30-AM.png",
+    headline: "Black hole dog"
+  },
+  {
+    src:
+      "https://static.twentytwowords.com/cdn-cgi/image/width=577,quality=85,fit=scale-down,format=auto,onerror=redirect/https://static.twentytwowords.com/wp-content/uploads/Screen-Shot-2018-05-03-at-11.12.08-AM.png",
+    headline: "Cool Alligator in Car"
+  }
+];
